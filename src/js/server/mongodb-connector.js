960966ecf4c441a076db89e3a6ad4204975d1efd
Kalimaha <guido.barbaglia@gmail.com>
2012-08-14 19:48:54 +0200

    Test

--- src/js/server/mongodb-connector.js
+++ src/js/server/mongodb-connector.js
@@ -37,4 +37,4 @@ app.get("/select/model/:title", function(req, res, next) {
 
 // Listen
 app.listen(3000);
-console.log('Listening on port 3000');
+console.log('Listening on port 3000 for MongoDB');
